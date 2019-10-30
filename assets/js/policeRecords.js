48a3ed4ab2f6a1b77fbdf686fba368029b68e264
Andy Durette <duretteandy@gmail.com>
2019-10-30 17:32:34 -0400

    nav-and-chart-update

--- assets/js/policeRecords.js
+++ assets/js/policeRecords.js
@@ -1,57 +1,72 @@
-    
- let callAssaults = (district) => {  
+let apiData;
+
+// To much info cannot store it to localStorage
+fetch("https://cors-anywhere.herokuapp.com/https://opendata.arcgis.com/datasets/98f7dde610b54b9081dfca80be453ac9_0.geojson")
+.then((resp) => resp.json())
+.then((data) => {
+  apiData = data.features;
+  console.log(apiData);
+  document.getElementById('loadingScreen').classList.add("hide");
+});
+
+
+/*}*/
+
+
+
+
+ let callMCI = (district) => {  
     console.log(district);
 
     let MCI = {
       
     }
   
-      fetch("https://cors-anywhere.herokuapp.com/https://opendata.arcgis.com/datasets/98f7dde610b54b9081dfca80be453ac9_0.geojson")
-      .then((resp) => resp.json())
-      .then((data) => {
-        Array.from(data.features).forEach((item) =>{
-          if ( item.properties.Division === district ){
-            if (MCI[`${item.properties.MCI}`]){
-              MCI[`${item.properties.MCI}`] = MCI[`${item.properties.MCI}`] + 1
-            }else{
-              MCI[`${item.properties.MCI}`] = 1
-            }
-          }
-      });
+
+    Array.from(apiData).forEach((item) =>{
+      if ( item.properties.Division === district ){
+        if (MCI[`${item.properties.MCI}`]){
+          MCI[`${item.properties.MCI}`] = MCI[`${item.properties.MCI}`] + 1
+        }else{
+          MCI[`${item.properties.MCI}`] = 1
+         }
+      }});
+
+    
 
       let crimeKeys = Object.keys(MCI);
       let crimeValues = Object.values(MCI);
        
-// Pie chart build Start
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'pie',
+// Bar chart
+new Chart(document.getElementById("myChart"), {
+  type: 'bar',
   data: {
     labels: crimeKeys,
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-        "#9b59b6",
-        "#f1c40f"
-      ],
-      data: crimeValues
-    }]
+    datasets: [
+      {
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: crimeValues
+      }
+    ]
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      hover: {mode: null},
+      events: []
+    }
   }
 });
 
+
+
 // Pie chart build End
-
-
-
-
 
 document.querySelector('.modal-guts h1').innerHTML = `Crime for district ${district.substring(1, 4)} since 2014`;
       
     document.getElementById("modal").classList.remove("hide");
-    document.getElementById("modalOverlay").classList.remove("hide");
-    }) 
+    document.getElementById("modalOverlay").classList.remove("hide"); 
    // Hide Modal
     document.getElementById('close-button').addEventListener("click", () => {
       document.getElementById("modal").classList.add("hide");
@@ -59,6 +74,48 @@ document.querySelector('.modal-guts h1').innerHTML = `Crime for district ${distr
     });
    
 }
+
+
+
+//  Nav js
+
+document.getElementById('myMenuFunction').onclick = function(e){
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Active Nav reassignment
+
+var activeItem = document.querySelectorAll("#myTopnav a");
+
+activeItem.forEach((userItem) => {
+  userItem.addEventListener("click",() => {
+    activeItem.forEach((userItem) => {
+      userItem.classList.remove("active");
+    })
+    userItem.classList.add("active");
+  })
+});
+
+
+
+
 
 
 
