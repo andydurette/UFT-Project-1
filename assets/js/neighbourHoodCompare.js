589156a7f4413086086321225c379b47990e83e2
Andy Durette <duretteandy@gmail.com>
2019-10-31 00:15:13 -0400

    Fixed chart alignment

--- assets/js/neighbourHoodCompare.js
+++ assets/js/neighbourHoodCompare.js
@@ -26,11 +26,11 @@ let neighbourHoodCompare = () =>{
 new Chart(document.getElementById("neighbourhoodChart"), {
   type: 'bar',
   data: {
-    labels: [50, 50, 50, 50, 50],
+    labels: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
     datasets: [
       {
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [50, 50, 50, 50, 50]
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3e95cd", "#8e5ea2","#3e95cd", "#8e5ea2","#3e95cd", "#8e5ea2","#3e95cd", "#8e5ea2"],
+        data: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
       }
     ]
   },
